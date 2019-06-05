ad9b0fcd5b7ffa891f219f38bbffb16827a68e2f
GitHubForDmitry <dmitrykoka@gmail.com>
2019-06-05 19:11:31 +0300

    sol

---
+++ main.js
@@ -0,0 +1,22 @@
+function getFirstDayInMonth(year, month, firstDay) {
+
+  year = new Date().getFullYear();
+  month = new Date().getMonth();
+  firstDay = new Date(year, month, 1);
+
+  return firstDay.getDay();
+}
+
+function getDaysOfTheWeek(el) {
+
+  el = document.querySelector('.calendar');
+  return el.setAttribute("class" , "calendar calendar-" + getFirstDayInMonth());
+}
+
+getDaysOfTheWeek();
+
+// let val = document.querySelector(".val");
+// let setMonth = document.querySelector(".month");
+// setMonth.onclick = function () {
+//   console.log(val.value);
+// }
